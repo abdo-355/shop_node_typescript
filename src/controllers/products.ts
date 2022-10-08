e9e75b0b985980f26f7f00a655e3d1ac2f48bf15
abdo-355 <abdoluxor366@gmail.com>
2022-10-08 02:24:43 +0200

    restructure the views folder

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express";
+import path from "path";
 
 import Product from "../models/product";
 
 export const getAddProduct: RequestHandler = (req, res, next) => {
-  res.render("add-product", {
+  res.render(path.join("admin", "add-product"), {
     pageTitle: "Add Product",
     path: "/admin/add-product",
   });
@@ -17,6 +18,10 @@ export const postAddProduct: RequestHandler = (req, res, next) => {
 
 export const getProducts: RequestHandler = (req, res, next) => {
   Product.fetchAll((products: Product[]) => {
-    res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+    res.render(path.join("shop", "product-list"), {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
   });
 };
