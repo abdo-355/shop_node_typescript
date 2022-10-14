92727aebc3d6512491cfe70ee1b67b36499fb36e
abdo-355 <abdoluxor366@gmail.com>
2022-10-14 22:53:23 +0200

    replace the product class with a product sequelize model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,44 +1,30 @@
-import db from "../util/database";
+import { DOUBLE, INTEGER, STRING } from "sequelize";
 
-import Cart from "./cart";
+import sequelize from "../util/database";
 
-class Product {
-  title: string;
-  imgUrl: string;
-  description: string;
-  price: string;
-  id: string | null;
-
-  constructor(
-    id: string | null,
-    title: string,
-    imgUrl: string,
-    description: string,
-    price: string
-  ) {
-    this.id = id;
-    this.title = title;
-    this.imgUrl = imgUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save = () => {
-    return db.execute(
-      "INSERT INTO products (title, imgUrl, description, price) VALUES (?, ?, ?, ?)",
-      [this.title, this.imgUrl, this.description, this.price]
-    );
-  };
-
-  static fetchAll = () => {
-    return db.execute("SELECT * FROM products");
-  };
-
-  static findProductById = (id: string) => {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  };
-
-  static deleteProduct = (id: string) => {};
-}
+const Product = sequelize.define("products", {
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DOUBLE,
+    allowNull: false,
+  },
+  imgUrl: {
+    type: STRING,
+    allowNull: false,
+  },
+  description: {
+    type: STRING,
+    allowNull: false,
+  },
+});
 
 export default Product;
