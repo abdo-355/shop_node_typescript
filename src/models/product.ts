264207b687ce809e6f3dd3049dbb217c6a4d316c
abdo-355 <abdoluxor366@gmail.com>
2022-10-28 23:51:49 +0200

    first steps in the products model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,67 +1,95 @@
-import { ObjectId } from "mongodb";
+import { Schema } from "mongoose";
 
-import { getDb } from "../util/database";
-
-class Product {
-  public title: string;
-  public price: number;
-  public imgUrl: string;
-  public description: string;
-  public userId: ObjectId;
-  public _id?: ObjectId;
-
-  constructor(
-    title: string,
-    price: number,
-    imgUrl: string,
-    description: string,
-    id: string | null,
-    userId: ObjectId
-  ) {
-    this.title = title;
-    this.price = price;
-    this.imgUrl = imgUrl;
-    this.description = description;
-    this.userId = userId;
-    if (id) {
-      this._id = new ObjectId(id);
-    }
-  }
-
-  public save = () => {
-    const db = getDb();
-    if (this._id) {
-      return db
-        ?.collection("products")
-        .updateOne({ _id: this._id }, { $set: this });
-    } else {
-      return db?.collection("products").insertOne(this);
-    }
-  };
-
-  public static fetchAll = () => {
-    const db = getDb();
-    return db?.collection("products").find().toArray();
-  };
-
-  public static findById = (productId: string) => {
-    const db = getDb();
-    return db
-      ?.collection("products")
-      .find({ _id: new ObjectId(productId) })
-      .next()
-      .then((product) => {
-        return product;
-      })
-      .catch((err) => console.log(err));
-  };
-
-  public static deletebyId = (productId: string) => {
-    const db = getDb();
-    return db
-      ?.collection("products")
-      .deleteOne({ _id: new ObjectId(productId) });
-  };
+interface IProduct {
+  title: string;
+  price: number;
+  imgUrl: string;
+  description: string;
 }
 
-export default Product;
+const productSchema = new Schema<IProduct>({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+// import { ObjectId } from "mongodb";
+
+// import { getDb } from "../util/database";
+
+// class Product {
+//   public title: string;
+//   public price: number;
+//   public imgUrl: string;
+//   public description: string;
+//   public userId: ObjectId;
+//   public _id?: ObjectId;
+
+//   constructor(
+//     title: string,
+//     price: number,
+//     imgUrl: string,
+//     description: string,
+//     id: string | null,
+//     userId: ObjectId
+//   ) {
+//     this.title = title;
+//     this.price = price;
+//     this.imgUrl = imgUrl;
+//     this.description = description;
+//     this.userId = userId;
+//     if (id) {
+//       this._id = new ObjectId(id);
+//     }
+//   }
+
+//   public save = () => {
+//     const db = getDb();
+//     if (this._id) {
+//       return db
+//         ?.collection("products")
+//         .updateOne({ _id: this._id }, { $set: this });
+//     } else {
+//       return db?.collection("products").insertOne(this);
+//     }
+//   };
+
+//   public static fetchAll = () => {
+//     const db = getDb();
+//     return db?.collection("products").find().toArray();
+//   };
+
+//   public static findById = (productId: string) => {
+//     const db = getDb();
+//     return db
+//       ?.collection("products")
+//       .find({ _id: new ObjectId(productId) })
+//       .next()
+//       .then((product) => {
+//         return product;
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   public static deletebyId = (productId: string) => {
+//     const db = getDb();
+//     return db
+//       ?.collection("products")
+//       .deleteOne({ _id: new ObjectId(productId) });
+//   };
+// }
+
+// export default Product;
