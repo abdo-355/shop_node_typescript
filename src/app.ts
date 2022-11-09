bf10c08dcf0ad88643dd52c713c5068dd5c54020
abdo-355 <abdoluxor366@gmail.com>
2022-11-09 20:50:24 +0200

    configure the file name and folder of multer

--- src/app.ts
+++ src/app.ts
@@ -23,12 +23,20 @@ const store = MongoStore.create({
   collectionName: "sessions",
 });
 const csrfProtection = csurf();
+const fileStorage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "images");
+  },
+  filename(req, file, cb) {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer({ dest: "images" }).single("image"));
+app.use(multer({ storage: fileStorage }).single("image"));
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(
   session({
