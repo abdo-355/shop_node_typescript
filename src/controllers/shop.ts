f0fc3aee754b9397acfdac5b2f49ebe84d49a8f6
abdo-355 <abdoluxor366@gmail.com>
2022-11-10 23:16:17 +0200

    replace reading then sending the file with streaming it

--- src/controllers/shop.ts
+++ src/controllers/shop.ts
@@ -150,14 +150,13 @@ export const getInvoice: RequestHandler = async (req, res, next) => {
 
     const invoiceName = "invoice-" + orderId + ".pdf";
     const invoicePath = path.join("invoices", invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        return next(err);
-      }
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
-      res.send(data);
-    });
+
+    const file = fs.createReadStream(invoicePath);
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `attach; filename="${invoiceName}"`);
+
+    file.pipe(res);
   } catch (err) {
     next(err);
   }
