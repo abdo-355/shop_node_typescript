73f26146723686fe8c01474571ba39b5f16d1a8a
abdo-355 <abdoluxor366@gmail.com>
2022-11-12 20:25:15 +0200

    remove the product from the ui when deleted using client side js

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,6 +2,7 @@ const deleteProduct = async (btn) => {
   try {
     const productId = btn.parentNode.querySelector("[name=productId]").value;
     const csrfToken = btn.parentNode.querySelector("[name=_csrf]").value;
+    const productElement = btn.closest("article");
 
     const result = await fetch(`/admin/product/${productId}`, {
       method: "DELETE",
@@ -10,7 +11,10 @@ const deleteProduct = async (btn) => {
       },
     });
 
-    console.log(result);
+    const data = await result.json();
+    console.log(data);
+
+    productElement.remove();
   } catch (err) {
     console.log(err);
   }
